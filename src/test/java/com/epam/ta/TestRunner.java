de7274eab465da0eec0855b76a4c88b4b2c32c95
Kornel Filep <kornel.filep@gmail.com>
2023-11-06 15:33:44 +0100

    add cucumber report

--- src/test/java/com/epam/ta/TestRunner.java
+++ src/test/java/com/epam/ta/TestRunner.java
@@ -8,7 +8,8 @@ import org.junit.runner.RunWith;
 @CucumberOptions(
     features = "src/test/resources",
     glue = {"com.epam.ta"},
-    plugin = {"pretty"}
+    stepNotifications = true,
+    plugin = {"pretty", "html:target/test-report.html"}
 )
 public class TestRunner {
 }

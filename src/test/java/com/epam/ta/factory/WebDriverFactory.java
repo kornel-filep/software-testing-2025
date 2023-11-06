2037c2cc1b6590db2323a0a286ba03f4a72b353a
Kornel Filep <kornel.filep@gmail.com>
2023-11-06 15:23:40 +0100

    add pipeline and headless option

--- src/test/java/com/epam/ta/factory/WebDriverFactory.java
+++ src/test/java/com/epam/ta/factory/WebDriverFactory.java
@@ -4,12 +4,15 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.springframework.stereotype.Component;
+import org.springframework.beans.factory.annotation.Value;
 
 import java.time.Duration;
 import java.util.Objects;
 
 public class WebDriverFactory {
+
+    @Value("${headless:false}")
+    private Boolean headless;
 
     private WebDriver driver;
 
@@ -23,14 +26,14 @@ public class WebDriverFactory {
     private WebDriver setUpWebDriver() {
         WebDriverManager.chromedriver().setup();
         WebDriver driver = new ChromeDriver(
-            new ChromeOptions().addArguments("--remote-allow-origins=*")
+            new ChromeOptions().setHeadless(headless).addArguments("--remote-allow-origins=*")
         );
         driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
         return driver;
     }
 
     public void tearDown() {
-        if(Objects.nonNull(driver)) {
+        if (Objects.nonNull(driver)) {
             driver.quit();
             driver = null;
         }
